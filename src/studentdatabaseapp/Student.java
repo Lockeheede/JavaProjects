3bb04c1ac6adebfee622446d2d84b77d437d9949
Lockeheede <prescott.lawani@gmail.com>
2020-05-08 18:50:14 -0500

    Finished the studentdatabaseapp. Next is the bank app

--- src/studentdatabaseapp/Student.java
+++ src/studentdatabaseapp/Student.java
@@ -7,7 +7,7 @@ public class Student {
 	private String lastName;
 	private int gradeYear;
 	private String studentID;
-	private String courses = null;
+	private String courses = "";
 	private int tuitionBalance = 0;
 	private int costOfCourse = 600;
 	private static int id = 1000;
@@ -26,9 +26,6 @@ public class Student {
 		this.gradeYear = in.nextInt();
 		
 		setStudentID();
-		
-		System.out.println(firstName + " " + lastName + " " + gradeYear + " " + studentID);
-
 	}
 	
 	//Give student 5-digit unique ID. First number is their grade level
@@ -42,23 +39,41 @@ public class Student {
 	//Student can enroll in the following courses:
 	public void enroll() {
 		do {
-			System.out.println("Enter course to enroll (Q to quit)");
+			System.out.print("Enter course to enroll (Q to quit): ");
 			Scanner in = new Scanner(System.in);
 			String course = in.nextLine();
 			if(!course.equals("Q")) {
-				courses = courses + "\n" + course;
+				courses += "\n " + course;
 				tuitionBalance = tuitionBalance + costOfCourse;
 			}
 			else { break;}
 		}while(1 != 0);
-		System.out.println("ENROLLED IN: " + courses);
-		System.out.println("TUITION BALANCE: $" + tuitionBalance);
 	}
 	//History 101, Mathematics 101, English 101, Chemistry 101, Computer Science 101 
 			 
 	//Each course costs $600 to enroll
 			
 	//Students should be able to view their balance and pay the tutition
+	public void viewBalance() {
+		System.out.println("TUITION BALANCE: $" + tuitionBalance);
+	}
+	
+	public void payTuition(){
+		viewBalance();
+		System.out.print("Enter your payment: $");
+		Scanner in = new Scanner(System.in);
+		int payment = in.nextInt();
+		tuitionBalance = tuitionBalance - payment;
+		System.out.println("Thank you for your payment of $" + payment);
+		viewBalance();
+	}
 			
 	//To see the status of a student, we should see their name, ID, courses enrolled and balance
+	public String showInfo() {
+		return "Name: " + firstName + " " + lastName +
+				"\nGrade Level: " + gradeYear +
+				"\nStudent ID: " + studentID +
+				"\nCourses Enrolled: " + courses +
+				"\nBalance: $" + tuitionBalance;
+	}
 }
--- src/studentdatabaseapp/StudentDatabaseApp.java
+++ src/studentdatabaseapp/StudentDatabaseApp.java
@@ -1,13 +1,26 @@
 package studentdatabaseapp;
 
+import java.util.Scanner;
+
 public class StudentDatabaseApp {
 
-	public static void main(String[] args) {
-		//Add new students to database
+	public static void main(String[] args) {		
+		System.out.print("Enter the number of new students to enroll: ");
+		Scanner in = new Scanner(System.in);
+		int numOfStudents = in.nextInt();
+		Student[] students = new Student[numOfStudents];
 		
-		Student s1 = new Student();
-		s1.enroll();
+		for (int n = 0; n < numOfStudents; n++) {
+			//Add new students to database
+			
+			students[n] = new Student();
+			students[n].enroll();
+			students[n].payTuition();
+		}
 		
+		for (int n = 0; n < numOfStudents; n++) {
+			System.out.println(students[n].showInfo());
+		}
 	}
 
 }
